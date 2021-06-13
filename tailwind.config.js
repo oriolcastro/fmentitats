660133f52b32a0f9750df3e6721b15cb6793a4ba
Oriol Castro <oriol.castroarnau@gmail.com>
2021-06-13 21:19:55 +0200

    fix: improve images naming
    remove unused animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,13 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   mode: 'jit',
   purge: ['./public/**/*.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {
       backgroundImage: (theme) => ({
-        'tiquets-section': "url('/images/background.png')",
-        'divider-1': "url('/images/concert.jpeg')",
-        'divider-2': "url('/images/fme_1.jpeg')",
-        'divider-3': "url('/images/fme_2.jpeg')",
+        'divider-1': "url('/images/background_1.jpeg')",
+        'divider-2': "url('/images/background_2.jpeg')",
+        'divider-3': "url('/images/background_3.jpeg')",
       }),
       fontFamily: {
         heading: ['GeometosRounded', ...defaultTheme.fontFamily.sans],
@@ -20,25 +19,6 @@ module.exports = {
         'fme-green': 'rgb(57, 131, 110)',
         'fme-yellow': 'rgb(229, 197, 107)',
         'fme-black': 'rgb(35,35,35)',
-      },
-      animation: {
-        blob: 'blob 9s infinite',
-      },
-      keyframes: {
-        blob: {
-          '0%': {
-            transform: 'translate(0px, 0px) scale(1)',
-          },
-          '33%': {
-            transform: 'translate(20px, -25px) scale(1.1)',
-          },
-          '66%': {
-            transform: 'translate(-10px, 10px) scale(0.9)',
-          },
-          '100%': {
-            transform: 'translate(0px, 0px) scale(1)',
-          },
-        },
       },
     },
   },
