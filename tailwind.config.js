5cb58bffe52e22068d30b1ef9b4d1df0a419a475
Oriol Castro <ocastro@gigapipe.com>
2023-06-04 10:30:10 +0200

    Version 2023 (#7)
    * chore: update dependencies and tailwind config
    
    * feat: update logos and images
    
    * chore: update fonts
    
    * chore: update video and remove files
    
    * feat: start updating content
    
    * feat: canvi logos, imatges fons, cartells
    
    * chore: add nvm config file
    
    * chore: fix build command
    
    * chore: update styles
    
    * feat: add more content, links and fix issue with video playback
    
    * fix: buchery font loazing
    
    * chore: update lock file
    
    * fix: blur filter in video hero
    
    * fix: second fix for blur filter
    
    * fix: last fix for blur
    
    * fix: reduce blur
    
    * feat: add colabs
    
    * chore: delete some images
    
    * fix: image issues
    
    * fix: heading styles
    
    * fix: remove broken fixed background attachment in safari

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  mode: 'jit',
-  purge: ['./public/**/*.html'],
-  darkMode: false,
+  content: ['./public/**/*.html'],
   theme: {
     extend: {
       backgroundImage: (theme) => ({
-        'divider-1': "url('/images/background_1.jpeg')",
-        'divider-2': "url('/images/background_2.jpeg')",
-        'divider-3': "url('/images/background_3.jpeg')",
+        'divider-1': "url('/images/background_1.webp')",
+        'divider-2': "url('/images/background_2.webp')",
+        'divider-3': "url('/images/background_3.webp')",
+        'divider-4': "url('/images/background_4.webp')",
       }),
       fontFamily: {
-        heading: ['GeometosRounded', ...defaultTheme.fontFamily.sans],
+        heading: ['Buchery', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         'fme-red': 'rgb(193, 73, 71)',
@@ -20,10 +19,11 @@ module.exports = {
         'fme-yellow': 'rgb(229, 197, 107)',
         'fme-black': 'rgb(35,35,35)',
       },
+      zIndex: {
+        100: '100',
+      },
     },
   },
-  variants: {
-    extend: {},
-  },
+
   plugins: [require('@tailwindcss/typography')],
 };
