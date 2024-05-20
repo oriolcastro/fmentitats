c36bbc935c086d249933812ff02d45bc7b90f68f
Oriol <oriol.castroarnau@gmail.com>
2024-05-20 11:43:31 +0200

    feat: add SSG

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
+import { defineConfig } from 'vite'
 import build from '@hono/vite-cloudflare-pages'
 import devServer from '@hono/vite-dev-server'
 import adapter from '@hono/vite-dev-server/cloudflare'
-import { defineConfig } from 'vite'
+import ssg from '@hono/vite-ssg'
 
 export default defineConfig({
   plugins: [
+    ssg({}),
     build({ emptyOutDir: true }),
     devServer({
       adapter,
