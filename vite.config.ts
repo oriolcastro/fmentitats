fb651899ec6fb3b92fe46c3ced615d78648bb7e6
Oriol <oriol.castroarnau@gmail.com>
2024-05-20 11:24:22 +0200

    chore: empty dist folder on build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
-    build(),
+    build({ emptyOutDir: true }),
     devServer({
       adapter,
       entry: 'src/index.tsx',
