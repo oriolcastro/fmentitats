2ba98338c7c7529351d6c7e6135f1551879e6784
Oriol <oriol.castroarnau@gmail.com>
2024-05-27 16:29:54 +0200

    core: remove unused code

--- src/components/HeroImageFader.tsx
+++ src/components/HeroImageFader.tsx
@@ -1,25 +1,22 @@
-import { html } from 'hono/html'
-
 export const HeroImagesFader = () => {
   return (
     <div class="absolute z-10 h-full w-full object-cover">
       <div
-        class="animate-fade bg-hero-1 absolute top-0 -z-10 h-full w-full bg-cover bg-center bg-no-repeat opacity-0"
+        class="absolute top-0 -z-10 h-full w-full animate-fade bg-hero-1 bg-cover bg-center bg-no-repeat opacity-0"
         style="animation-delay:0s"
       ></div>
       <div
-        class="animate-fade bg-hero-2 absolute top-0 -z-10 h-full w-full bg-cover bg-center bg-no-repeat opacity-0"
+        class="absolute top-0 -z-10 h-full w-full animate-fade bg-hero-2 bg-cover bg-center bg-no-repeat opacity-0"
         style="animation-delay:4s"
       ></div>
       <div
-        class="animate-fade bg-hero-3 absolute top-0 -z-10 h-full w-full bg-cover bg-center bg-no-repeat opacity-0"
+        class="absolute top-0 -z-10 h-full w-full animate-fade bg-hero-3 bg-cover bg-center bg-no-repeat opacity-0"
         style="animation-delay:8s"
       ></div>
       <div
-        class="animate-fade bg-hero-4 absolute top-0 -z-10 h-full w-full bg-cover bg-center bg-no-repeat opacity-0"
+        class="absolute top-0 -z-10 h-full w-full animate-fade bg-hero-4 bg-cover bg-center bg-no-repeat opacity-0"
         style="animation-delay:12s"
       ></div>
-      {html` <style></style> `}
     </div>
   )
 }
