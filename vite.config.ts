ed698f12d6a03b93e15c9b02610f321691295de4
Oriol <oriol.castroarnau@gmail.com>
2024-05-31 18:30:10 +0200

    fix: config preventing correct build

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig(({ mode }) => {
             adapter,
           },
         }),
-        pages({ emptyOutDir: true }),
+        pages(),
         ssg({ entry }),
       ],
     }
