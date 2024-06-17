1a4d03a26192dd56c50a99a53cfc6bbc038b663b
Oriol <oriol.castroarnau@gmail.com>
2024-06-17 10:06:43 +0200

    feat: add sponsors for 2024

--- app/components/2024/Agenda.tsx
+++ app/components/2024/Agenda.tsx
@@ -188,27 +188,9 @@ export const Agenda: FC = () => {
         <div class="flex-1">
           <h3 class="my-2 font-heading text-xl">La música de la FME</h3>
           <p class="prose mb-4 text-justify">
-            Teniu una llista de reproducció amb els èxits de tots els artistes que actuaran a la Festa Major d'Entitats
-            d'aquest any.
+            Descobreix la música de tots els grups que actuaran a la Festa Major d'Entitats d'aquest any.
           </p>
-          <a
-            href="https://open.spotify.com/playlist/2VRhbvrgsyKc33vDc4PoCw?si=50e3a2485f2a4ebf"
-            target="_blank"
-            rel="noopener"
-            class="mx-auto mb-8 flex w-[fit-content] flex-row items-center rounded-md bg-fme-green px-4 py-3 text-white"
-          >
-            <svg
-              width="24"
-              height="24"
-              xmlns="http://www.w3.org/2000/svg"
-              class="mr-2 fill-current text-white"
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-            >
-              <path d="M19.098 10.638c-3.868-2.297-10.248-2.508-13.941-1.387-.593.18-1.22-.155-1.399-.748-.18-.593.154-1.22.748-1.4 4.239-1.287 11.285-1.038 15.738 1.605.533.317.708 1.005.392 1.538-.316.533-1.005.709-1.538.392zm-.126 3.403c-.272.44-.847.578-1.287.308-3.225-1.982-8.142-2.557-11.958-1.399-.494.15-1.017-.129-1.167-.623-.149-.495.13-1.016.624-1.167 4.358-1.322 9.776-.682 13.48 1.595.44.27.578.847.308 1.286zm-1.469 3.267c-.215.354-.676.465-1.028.249-2.818-1.722-6.365-2.111-10.542-1.157-.402.092-.803-.16-.895-.562-.092-.403.159-.804.562-.896 4.571-1.045 8.492-.595 11.655 1.338.353.215.464.676.248 1.028zm-5.503-17.308c-6.627 0-12 5.373-12 12 0 6.628 5.373 12 12 12 6.628 0 12-5.372 12-12 0-6.627-5.372-12-12-12z" />
-            </svg>
-            Llista FME 2024
-          </a>
+
           <div class="grid grid-cols-2 gap-4 lg:grid-cols-3 lg:gap-6">
             <a
               href="https://open.spotify.com/artist/08VkVB0giqumfUMl4Ea922?si=tkjBVsbTR8-Tfd-QtZX6nA"
@@ -302,18 +284,38 @@ export const Agenda: FC = () => {
             </a>
           </div>
         </div>
-        <picture>
-          <source srcset="static/images/2024/cartell2024.avif" type="image/avif" />
-          <source srcset="static/images/2024/cartell2024.webp" type="image/webp" />
-          <img
-            src="static/images/2024/cartell2024.jpg"
-            loading="lazy"
-            alt="Cartell dels concerts de l'edició 2024"
-            width="420"
-            height="525"
-            class="mt-12"
-          />
-        </picture>
+        <div class="flex flex-col items-center gap-4">
+          <picture>
+            <source srcset="static/images/2024/cartell2024.avif" type="image/avif" />
+            <source srcset="static/images/2024/cartell2024.webp" type="image/webp" />
+            <img
+              src="static/images/2024/cartell2024.jpg"
+              loading="lazy"
+              alt="Cartell dels concerts de l'edició 2024"
+              width="420"
+              height="525"
+              class="mt-12"
+            />
+          </picture>
+          <a
+            href="https://open.spotify.com/playlist/2VRhbvrgsyKc33vDc4PoCw?si=50e3a2485f2a4ebf"
+            target="_blank"
+            rel="noopener"
+            class="mx-auto mb-8 flex w-[fit-content] flex-row items-center rounded-md bg-fme-green px-4 py-3 text-white"
+          >
+            <svg
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+              class="mr-2 fill-current text-white"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            >
+              <path d="M19.098 10.638c-3.868-2.297-10.248-2.508-13.941-1.387-.593.18-1.22-.155-1.399-.748-.18-.593.154-1.22.748-1.4 4.239-1.287 11.285-1.038 15.738 1.605.533.317.708 1.005.392 1.538-.316.533-1.005.709-1.538.392zm-.126 3.403c-.272.44-.847.578-1.287.308-3.225-1.982-8.142-2.557-11.958-1.399-.494.15-1.017-.129-1.167-.623-.149-.495.13-1.016.624-1.167 4.358-1.322 9.776-.682 13.48 1.595.44.27.578.847.308 1.286zm-1.469 3.267c-.215.354-.676.465-1.028.249-2.818-1.722-6.365-2.111-10.542-1.157-.402.092-.803-.16-.895-.562-.092-.403.159-.804.562-.896 4.571-1.045 8.492-.595 11.655 1.338.353.215.464.676.248 1.028zm-5.503-17.308c-6.627 0-12 5.373-12 12 0 6.628 5.373 12 12 12 6.628 0 12-5.372 12-12 0-6.627-5.372-12-12-12z" />
+            </svg>
+            Llista FME 2024
+          </a>
+        </div>
       </div>
     </section>
   )
--- app/components/2024/Sponsors.tsx
+++ app/components/2024/Sponsors.tsx
@@ -9,16 +9,227 @@ export const Sponsors: FC = () => {
         amb el projecte.
       </p>
       <div class="grid grid-cols-2 items-center justify-items-center gap-2 md:grid-cols-4">
-        {/* <!-- <picture>
-          <source srcset="/static/images/esponsors/aperitius_tafaner.webp" type="image/webp" />
+        <picture>
+          <source srcset="/static/images/2024/esponsors/aperitius_tafaner.webp" type="image/webp" />
           <img
-            src="/static/images/esponsors/aperitius_tafaner.png"
+            src="/static/images/2024/esponsors/aperitius_tafaner.png"
             loading="lazy"
             alt="Aperitius Tafaner"
             width="200"
             height="200"
           />
-        </picture> --> */}
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/estrella_galicia.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/estrella_galicia.png"
+            loading="lazy"
+            alt="Estrella Galicia"
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/dvp.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/dvp.png"
+            loading="lazy"
+            alt="Distribucio i vending Penedes"
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/gin_mg.webp" type="image/webp" />
+          <img src="/static/images/2024/esponsors/gin_mg.png" loading="lazy" alt="Gin MG" width="200" height="200" />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/paradistes_mercatt.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/paradistes_mercatt.jpg"
+            loading="lazy"
+            alt="Associacio Paradistes del Mercat"
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/arcon_de_alba.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/arcon_de_alba.jpg"
+            loading="lazy"
+            alt="Arcon de Alba"
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/bacallaneria_toni.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/bacallaneria_toni.png"
+            loading="lazy"
+            alt="Bacallaneria Toni"
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/bar_del_mercat.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/bar_del_mercat.jpg"
+            loading="lazy"
+            alt="Bar del Mercat"
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/can_batadet.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/can_batadet.png"
+            loading="lazy"
+            alt="Can Batadet"
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/carpeta_moderna.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/carpeta_moderna.jpg"
+            loading="lazy"
+            alt="Carpeta Moderna"
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/fisio_lab.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/fisio_lab.png"
+            loading="lazy"
+            alt="Fisio Lab"
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/freilka.webp" type="image/webp" />
+          <img src="/static/images/2024/esponsors/freilka.png" loading="lazy" alt="Freilka" width="200" height="200" />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/la_cava.webp" type="image/webp" />
+          <img src="/static/images/2024/esponsors/la_cava.png" loading="lazy" alt="La Cava" width="200" height="200" />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/lotus.webp" type="image/webp" />
+          <img src="/static/images/2024/esponsors/lotus.png" loading="lazy" alt="Lotus" width="200" height="200" />
+        </picture>
+
+        <picture>
+          <source srcset="/static/images/2024/esponsors/marta_carnisseria.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/marta_carnisseria.jpg"
+            loading="lazy"
+            alt="Marta Carnisseria"
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/peixosdolors.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/peixosdolors.jpg"
+            loading="lazy"
+            alt="Peixos Dolors"
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/ganiveteria_rodriguez.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/ganiveteria_rodriguez.png"
+            loading="lazy"
+            alt="Ganiveteria Rodriguez"
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/bar_italia.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/bar_italia.png"
+            loading="lazy"
+            alt="Bar Italia"
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/el_celler.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/el_celler.png"
+            loading="lazy"
+            alt="El Celler "
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/la_gralla.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/la_gralla.png"
+            loading="lazy"
+            alt="La Gralla"
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/la_lluna.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/la_lluna.png"
+            loading="lazy"
+            alt="La Lluna"
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/la_tasca.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/la_tasca.png"
+            loading="lazy"
+            alt="La Tasca "
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/supermercats_jr.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/supermercats_jr.png"
+            loading="lazy"
+            alt="Supermercats JR "
+            width="200"
+            height="200"
+          />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/lona.webp" type="image/webp" />
+          <img src="/static/images/2024/esponsors/lona.png" loading="lazy" alt="l'Ona" width="200" height="200" />
+        </picture>
+        <picture>
+          <source srcset="/static/images/2024/esponsors/la_parroquia.webp" type="image/webp" />
+          <img
+            src="/static/images/2024/esponsors/la_parroquia.png"
+            loading="lazy"
+            alt="La Parroquia"
+            width="200"
+            height="200"
+          />
+        </picture>
       </div>
     </section>
   )
