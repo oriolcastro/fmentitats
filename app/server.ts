5069aa9230e5c8c0153c57a2d734f8aec11085b0
Oriol <oriol.castroarnau@gmail.com>
2025-04-24 15:14:26 +0200

    feat: test addition of esponsors url to host the info

--- app/server.ts
+++ app/server.ts
@@ -3,6 +3,12 @@ import { showRoutes } from 'hono/dev'
 
 const app = createApp()
 
+app.get('/esponsors', c => {
+  return c.json({
+    'your name is': 'FMEntitats',
+  })
+})
+
 showRoutes(app)
 
 export default app
